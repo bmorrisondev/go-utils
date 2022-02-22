7e541dc34ef0a437af45a5dd721527468d349c50
Brian Morrison II <brian@brianmorrison.me>
2022-02-22 11:50:42 -0600

    🎉  Initial commit

---
+++ aws/dynamo.go
@@ -0,0 +1,59 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/pkg/errors"
+)
+
+type DynamoContext struct {
+	DynamoSvc *dynamodb.DynamoDB
+	Session   *session.Session
+	TableName *string
+}
+
+func NewDynamoContext(tableName string, sess *session.Session) (*DynamoContext, error) {
+	if tableName == "" {
+		return nil, errors.New("(NewDynamoContext) tableName is required")
+	}
+	var _session *session.Session
+	if sess == nil {
+		sess, err := session.NewSession()
+		if err != nil {
+			return nil, errors.Wrap(err, "(MakeContext) creating session")
+		}
+		_session = sess
+	} else {
+		_session = sess
+	}
+
+	svc := dynamodb.New(_session)
+
+	context := DynamoContext{
+		DynamoSvc: svc,
+		Session:   _session,
+	}
+
+	context.TableName = &tableName
+	return &context, nil
+}
+
+func (context *DynamoContext) Put(obj interface{}) error {
+	item, err := dynamodbattribute.MarshalMap(obj)
+	if err != nil {
+		return errors.Wrap(err, "(PutBuildToDynamo) Marshal map")
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: context.TableName,
+	}
+
+	_, err = context.DynamoSvc.PutItem(input)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ aws/lambda.go
@@ -0,0 +1,49 @@
+package aws
+
+import (
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/pkg/errors"
+)
+
+func ErrorResponse(err error, message string) (events.APIGatewayProxyResponse, error) {
+	err = errors.Wrap(err, message)
+	log.Println(err)
+	errStr := err.Error()
+	return makeResponse(&errStr, 500)
+}
+
+func UnauthorizedResponse(body *string) (events.APIGatewayProxyResponse, error) {
+	return makeResponse(body, 401)
+}
+
+func OkResponse(body *string) (events.APIGatewayProxyResponse, error) {
+	return makeResponse(body, 200)
+}
+
+func CreatedResponse(body *string) (events.APIGatewayProxyResponse, error) {
+	return makeResponse(body, 201)
+}
+
+func BadRequestResponse(body *string) (events.APIGatewayProxyResponse, error) {
+	return makeResponse(body, 400)
+}
+
+func makeResponse(body *string, statusCode int) (events.APIGatewayProxyResponse, error) {
+	res := events.APIGatewayProxyResponse{
+		Headers: map[string]string{
+			"Content-Type":                 "application/json",
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "*",
+			"Access-Control-Allow-Headers": "*",
+		},
+		StatusCode: statusCode,
+	}
+
+	if body != nil {
+		res.Body = *body
+	}
+
+	return res, nil
+}
